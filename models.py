78f46fdfa857b1acdb5960d6fdcfe94ab98a7319
jkzhang7 <jkzhang7@hotmail.com>
2019-12-08 14:41:51 -0800

    refreshing

---
+++ models.py
@@ -0,0 +1,74 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+import datetime
+import pdb
+import time
+import torchvision.models as torchmodels
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S_log.txt')
+        # self.logFile = open('C:/Users/Dell/Desktop/cse455/vision-hw5/logs/' + st, 'w')
+        self.logFile = open('C:/Users/Dell/Desktop/cse455/vision-hw5/logs/boringnet.txt', 'w')
+
+    def log(self, str):
+        print(str)
+        self.logFile.write(str + '\n')
+
+    def criterion(self):
+        return nn.CrossEntropyLoss()
+
+    def optimizer(self):
+        return optim.SGD(self.parameters(), lr=0.001)
+
+    def adjust_learning_rate(self, optimizer, epoch, args):
+        lr = args.lr  # TODO: Implement decreasing learning rate's rules
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+       
+
+
+class LazyNet(BaseModel):
+    def __init__(self):
+        super(LazyNet, self).__init__()
+        # TODO: Define model here
+        self.fc1 = nn.Linear(32 * 32 * 3, 10)
+
+    def forward(self, x):
+        # TODO: Implement forward pass for LazyNet
+        x = x.view(-1, 32 * 32 *3)
+        x = self.fc1(x)
+        return x
+        
+
+class BoringNet(BaseModel):
+    def __init__(self):
+        super(BoringNet, self).__init__()
+        self.fc1 = nn.Linear(32 * 32 * 3, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+        # TODO: Define model here
+
+    def forward(self, x):
+        # TODO: Implement forward pass for BoringNet
+        x = x.view(-1, 32 * 32 * 3)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+
+class CoolNet(BaseModel):
+    def __init__(self):
+        super(CoolNet, self).__init__()
+        # TODO: Define model here
+
+    def forward(self, x):
+        # TODO: Implement forward pass for CoolNet
+        return x
---
+++ plot_progress.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import pdb
+import argparse
+
+
+def argParser():
+	parser = argparse.ArgumentParser(description='PyTorch Plot Progress')
+	parser.add_argument('--file_name', default='')
+	return parser.parse_args()
+
+
+def main():
+	args = argParser()
+	train_accuracy=[]
+	test_accuracy=[]
+	train_loss=[]
+	with open('C:/Users/Dell/Desktop/cse455/vision-hw5/logs/boring.txt') as f:
+		for line in f:
+			if 'Final Summary' in line:
+				train_loss.append(float(line[:-1].split(' ')[-1]))
+			elif 'Train Accuracy of the network' in line:
+				train_accuracy.append(float(line[:-1].split(' ')[-2]))
+			elif 'Test Accuracy of the network' in line:
+				test_accuracy.append(float(line[:-1].split(' ')[-2]))
+
+	
+	plt.plot(train_accuracy)
+	plt.plot(test_accuracy)
+	plt.show()
+	# plt.savefig('C:/Users/Dell/Desktop/cse455/vision-hw5/logs/accu.png')
+	plt.plot(train_loss)
+	plt.show()
+	# plt.savefig('C:/Users/Dell/Desktop/cse455/vision-hw5/logs/loss.png')
+
+
+
+if __name__ == '__main__':
+	main()
